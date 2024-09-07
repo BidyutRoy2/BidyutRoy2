fe7bb4dae99ac6b810f25c0ed7efae3f3b72c9cf
Hidden Gem - Bidyut Roy <74731201+BidyutRoy2@users.noreply.github.com>
2024-09-08 04:16:39 +0600

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -9,22 +9,13 @@ ENDCOLOR="\e[0m"
 
 colors
 
-echo -e "${YELLOW}##   ##   ######  #####    #####    #######  ##    ##${ENDCOLOR}"
-echo -e "${YELLOW}##   ##     ##    ##  ##   ##  ##   ##       ###   ##${ENDCOLOR}"
-echo -e "${YELLOW}##   ##     ##    ##   ##  ##   ##  ##       ## #  ##${ENDCOLOR}"
-echo -e "${YELLOW}#######     ##    ##   ##  ##   ##  #####    ##  # ##${ENDCOLOR}"  
-echo -e "${YELLOW}##   ##     ##    ##   ##  ##   ##  ##       ##   ###${ENDCOLOR}"
-echo -e "${YELLOW}##   ##     ##    ##  ##   ##  ##   ##       ##    ##${ENDCOLOR}"
-echo -e "${YELLOW}##   ##   ######  #####    #####    #######  ##    ##${ENDCOLOR}"
-
-echo -e "${YELLOW}           #####     #######  ##     ##              ${ENDCOLOR}"
-echo -e "${YELLOW}          ##   ##    ##       ###   ###              ${ENDCOLOR}"
-echo -e "${YELLOW}          ##         ##       ## # # ##              ${ENDCOLOR}"
-echo -e "${YELLOW}          ##  #####  #####    ##  #  ##              ${ENDCOLOR}"
-echo -e "${YELLOW}          ##   ## #  ##       ##     ##              ${ENDCOLOR}"
-echo -e "${YELLOW}          ##   ## #  ##       ##     ##              ${ENDCOLOR}"
-echo -e "${YELLOW}           #####     #######  ##     ##              ${ENDCOLOR}"
+echo -e "${YELLOW}██╗  ██╗██╗██████╗ ██████╗ ███████╗███╗   ██╗     ██████╗ ███████╗███╗   ███╗${ENDCOLOR}"
+echo -e "${YELLOW}██║  ██║██║██╔══██╗██╔══██╗██╔════╝████╗  ██║    ██╔════╝ ██╔════╝████╗ ████║${ENDCOLOR}"
+echo -e "${YELLOW}███████║██║██║  ██║██║  ██║█████╗  ██╔██╗ ██║    ██║  ███╗█████╗  ██╔████╔██║${ENDCOLOR}"
+echo -e "${YELLOW}██╔══██║██║██║  ██║██║  ██║██╔══╝  ██║╚██╗██║    ██║   ██║██╔══╝  ██║╚██╔╝██║${ENDCOLOR}"
+echo -e "${YELLOW}██║  ██║██║██████╔╝██████╔╝███████╗██║ ╚████║    ╚██████╔╝███████╗██║ ╚═╝ ██║${ENDCOLOR}"
+echo -e "${YELLOW}╚═╝  ╚═╝╚═╝╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝     ╚═════╝ ╚══════╝╚═╝     ╚═╝${ENDCOLOR}" 
 
 echo -e "${BOLD}${YELLOW} Join Telegram: https://t.me/hiddengemnews ${ENDCOLOR}"
 
-echo -e "${BOLD}${YELLOW} Follow Github Get More Node Installation Guide ${ENDCOLOR}"
+echo -e "${BOLD}${YELLOW} Follow Github Get More Node Installation Guide ${ENDCOLOR}"                                                                      
