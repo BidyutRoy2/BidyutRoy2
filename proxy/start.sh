922e8efbb90869b4ff97e1fb3d5486de53a858ab
Hidden Gem - Bidyut Roy <74731201+BidyutRoy2@users.noreply.github.com>
2025-12-20 03:21:41 +0600

    Delete proxy directory

--- proxy/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chmod +x core/*.sh
-./core/fetch.sh
-./core/merge.sh
-./core/check_alive.sh
-echo "✅ Finished: Live proxies saved in proxyresult/"
-
--- proxy/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./core/fetch.sh
-./core/merge.sh
-./core/check_alive.sh
-echo "♻️ Proxies updated successfully"
-
