0e6e657fe1f6b1a703b4b53501f4ba9203ce39f6
Hidden Gem - Bidyut Roy <74731201+BidyutRoy2@users.noreply.github.com>
2025-12-20 01:38:24 +0600

    Update merge.sh

--- proxy/core/merge.sh
+++ proxy/core/merge.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+mkdir -p proxyresult tmp
+
+find sources -type f -name "*.txt" -exec cat {} + > tmp/all.txt
+
+grep -iE '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+' tmp/all.txt | sort -u > tmp/clean.txt
+
+grep -i socks5 tmp/clean.txt > proxyresult/socks5.txt
+grep -i socks4 tmp/clean.txt > proxyresult/socks4.txt
+grep -iv socks tmp/clean.txt > proxyresult/http.txt
 
